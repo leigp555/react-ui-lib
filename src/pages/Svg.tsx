43d8b5bc0524eefb79e216852685b1908d8931ca
leigp <907090585@qq.com>
2022-07-12 20:06:16 +0800

    docs: 展示多个svg

--- src/pages/Svg.tsx
+++ src/pages/Svg.tsx
@@ -1,15 +1,18 @@
 import React,{ CSSProperties } from 'react';
 import Wechat from '../static/wechat.svg'
+import QQ from '../static/qq.svg'
 
 const Svg:React.FC<{}>=() => {
   const logoStyle:CSSProperties = {
-    fill: '#CF4532',
-    width: '100px'
+    fill: 'green',
+    width: '30px',
+    height: '30px'
   };
   return (
     <>
       <div>svg</div>
       <Wechat style={logoStyle} />
+      <QQ style={logoStyle} />
     </>
 
 
