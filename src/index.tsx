480a73c121f4683100184716e4f4f4b6df88ef9b
leigp <907090585@qq.com>
2022-07-14 20:20:49 +0800

    docs: 更改路由模式为hash模式

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'nprogress/nprogress.css';
 import './style/index.scss';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
