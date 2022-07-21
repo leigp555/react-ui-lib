fbc5a6c3678f65f9fbcc5a22889ae33a0bfb4396
leigp <907090585@qq.com>
2022-07-22 01:39:28 +0800

    feat: 添加示例

---
+++ src/lib/Radio/example.tsx
@@ -0,0 +1,45 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+// import Radio from '../lib/Radio/Radio';
+// import RadioItem from '../lib/Radio/RadioItem';
+//
+// const Wrap = styled.div`
+//   position: relative;
+//   display: flex;
+//   gap: 50px;
+//   flex-direction: column;
+//   width: 350px;
+// `;
+//
+// const Home: React.FC = () => {
+//   const [value, setValue] = useState(2);
+//   const [value2, setValue2] = useState(4);
+//   const callback = (num: number) => {
+//     console.log('xx', num);
+//     setValue(num);
+//   };
+//   const callback2 = (num: number) => {
+//     console.log('yy', num);
+//     setValue2(num);
+//   };
+//   return (
+//     <Wrap>
+//       <Radio callback={callback} value={value} direction="row">
+//         <RadioItem value={1}>A</RadioItem>
+//         <RadioItem value={2}>B</RadioItem>
+//         <RadioItem value={3}>C</RadioItem>
+//         <RadioItem value={4}>D</RadioItem>
+//       </Radio>
+//       <Radio callback={callback2} value={value2} direction="column">
+//         <RadioItem value={1}>A</RadioItem>
+//         <RadioItem value={2}>B</RadioItem>
+//         <RadioItem value={3}>C</RadioItem>
+//         <RadioItem value={4}>D</RadioItem>
+//         <RadioItem value={5}>E</RadioItem>
+//       </Radio>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
