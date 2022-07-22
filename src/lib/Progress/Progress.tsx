ec4405848603fe0329b9eab462b45e0c827c43f1
leigp <907090585@qq.com>
2022-07-23 05:40:08 +0800

    feat: 添加圆形进度条

---
+++ src/lib/Progress/Progress.tsx
@@ -0,0 +1,66 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import './index.scss';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  percent: number;
+  finishColor?: string;
+  undoneColor?: string;
+  children?: React.ReactNode;
+}
+
+const Wrap = styled.div`
+  display: inline-block;
+  position: relative;
+
+  > .inner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10;
+  }
+`;
+type ProgressStyledProp = {
+  percent: number;
+  finishColor: string;
+  undoneColor: string;
+};
+const ProgressStyled = styled.div`
+  display: inline-block;
+
+  > .shadow {
+    width: 100px;
+    height: 100px;
+    position: relative;
+    border-radius: 50%;
+    background: ${(props: ProgressStyledProp) =>
+      `conic-gradient(${props.finishColor} 0%, ${props.finishColor} ${props.percent}%, ${props.undoneColor} ${props.percent}%, ${props.undoneColor} 100%)`};
+    mask: radial-gradient(transparent 40px, #000 41px);
+  }
+`;
+const Progress: React.FC<Props> = (props) => {
+  const { children, percent, finishColor, undoneColor, ...rest } = props;
+  return (
+    <div>
+      <Wrap>
+        <ProgressStyled
+          {...rest}
+          percent={percent}
+          finishColor={finishColor!}
+          undoneColor={undoneColor!}
+        >
+          <div className="shadow" />
+        </ProgressStyled>
+        <div className="inner" style={{ fontWeight: 700 }}>{`${percent}%`}</div>
+      </Wrap>
+    </div>
+  );
+};
+Progress.defaultProps = {
+  children: '',
+  finishColor: '#1890ff',
+  undoneColor: '#666666'
+};
+
+export default Progress;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Result from '../lib/Result/Result';
-import Button from '../lib/Button/Button';
+import Progress from '../lib/Progress/Progress';
 
 const Wrap = styled.div`
   display: flex;
@@ -12,58 +11,10 @@ const Wrap = styled.div`
 
 const Home: React.FC = () => {
   return (
-    <>
-      <Wrap>
-        <Result
-          status="success"
-          title="Successfully Purchased Cloud Server ECS!"
-          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-        >
-          <Button type="primary" key="console">
-            Back
-          </Button>
-          <Button key="buy">Next</Button>
-        </Result>
-      </Wrap>
-      <Wrap>
-        <Result
-          status="warning"
-          title="Your operation has been executed"
-          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-        >
-          <Button type="primary" key="console">
-            Back
-          </Button>
-          <Button key="buy">Close</Button>
-        </Result>
-      </Wrap>
-      <Wrap>
-        <Result
-          gap={40}
-          status="error"
-          title="There are some problems with your operation"
-          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-        >
-          <Button type="primary" key="console">
-            Back
-          </Button>
-          <Button key="buy">Close</Button>
-        </Result>
-      </Wrap>
-      <Wrap>
-        <Result
-          gap={20}
-          status="404"
-          title="Your operation has been executed"
-          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-        >
-          <Button type="primary" key="console">
-            Back
-          </Button>
-          <Button key="buy">Close</Button>
-        </Result>
-      </Wrap>
-    </>
+    <Wrap>
+      <Progress percent={40} />
+      <Progress percent={60} finishColor="red" undoneColor="green" />
+    </Wrap>
   );
 };
 
