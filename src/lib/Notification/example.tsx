49dc22688a71d2e5ffddf1fdae5fac369ed877d9
leigp <907090585@qq.com>
2022-07-23 22:18:17 +0800

    feat: 添加notification组件示例

---
+++ src/lib/Notification/example.tsx
@@ -0,0 +1,37 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import styled from 'styled-components';
+// import Notification, { openNotification } from '../lib/Notification/Notification';
+//
+// import Button from '../lib/Button/Button';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 50px;
+// `;
+//
+// const Home: React.FC = () => {
+//   const open = () => {
+//     openNotification(
+//       <Notification>
+//         <p>Notification Title</p>
+//         <div>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda cum ex
+//           explicabo fuga, illo ipsa laudantium libero magni minus nobis nostrum, odio quam quibusdam
+//           recusandae saepe soluta totam voluptates?
+//         </div>
+//       </Notification>,
+//       3000
+//     );
+//   };
+//   return (
+//     <Wrap>
+//       <div>
+//         <Button onClick={open}>按钮</Button>
+//       </div>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
