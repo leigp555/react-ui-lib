e0abae87f7f2009373457373eb22bae49f1c7759
leigp <907090585@qq.com>
2022-07-23 09:05:45 +0800

    style: 添加示例

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,68 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-import PopConfirm from '../lib/Popconfirm/Popconfirm';
-import Button from '../lib/Button/Button';
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 300px;
-  padding: 200px;
+  gap: 50px;
 `;
 
 const Home: React.FC = () => {
-  const confirm = () => {
-    console.log('confirm');
-  };
-
-  const cancel = () => {
-    console.log('cancel');
-  };
-
-  return (
-    <Wrap>
-      <PopConfirm
-        title="Are you sure to delete this task?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-        position="top"
-      >
-        <Button type="link">delete</Button>
-      </PopConfirm>
-      <PopConfirm
-        title="Are you sure to delete this task?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-        position="bottom"
-      >
-        <Button type="link">delete</Button>
-      </PopConfirm>
-      <PopConfirm
-        title="Are you sure to delete this task?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-        position="left"
-      >
-        <Button type="link">delete</Button>
-      </PopConfirm>
-      <PopConfirm
-        title="Are you sure to delete this task?"
-        onConfirm={confirm}
-        onCancel={cancel}
-        okText="Yes"
-        cancelText="No"
-        position="right"
-      >
-        <Button type="link">delete</Button>
-      </PopConfirm>
-    </Wrap>
-  );
+  return <Wrap>xxx</Wrap>;
 };
 
 export default Home;
