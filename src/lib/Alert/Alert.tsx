570e7a5bf2081b2cfc8da8367bd8011934e8e3a4
leigp <907090585@qq.com>
2022-07-24 01:15:05 +0800

    feat: 完成alert组件

---
+++ src/lib/Alert/Alert.tsx
@@ -0,0 +1,80 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import SuccessIcon from '../icons/success.svg';
+import InfoIcon from '../icons/info.svg';
+import WarningIcon from '../icons/warning.svg';
+import ErrorIcon from '../icons/error.svg';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  type?: 'success' | 'info' | 'warning' | 'error';
+  defineIcon?: React.ReactNode;
+}
+const AlertStyled = styled.div`
+  max-width: 400px;
+  min-width: 200px;
+  padding: 10px 15px;
+  display: inline-flex;
+  align-items: start;
+  gap: 10px;
+  &.success {
+    background-color: #e6f7ff;
+    border: 1px solid #91d5ff;
+  }
+  &.info {
+    background-color: #f6ffed;
+    border: 1px solid #b7eb8f;
+  }
+  &.warning {
+    background-color: #fffbe6;
+    border: 1px solid #ffe58f;
+  }
+  &.error {
+    background-color: #fff2f0;
+    border: 1px solid #ffccc7;
+  }
+  > .icon {
+    display: flex;
+    align-items: center;
+  }
+  > .content {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Alert: React.FC<Props> = (props) => {
+  const { children, type, defineIcon, ...rest } = props;
+  const render = () => {
+    if (type === 'success' && !defineIcon) {
+      return <SuccessIcon width="1em" height="1em" />;
+    }
+    if (type === 'info') {
+      return <InfoIcon width="1em" height="1em" />;
+    }
+    if (type === 'warning') {
+      return <WarningIcon width="1em" height="1em" />;
+    }
+    if (type === 'error') {
+      return <ErrorIcon width="1em" height="1em" />;
+    }
+    if (defineIcon) {
+      return defineIcon;
+    }
+  };
+  return (
+    <div>
+      <AlertStyled className={type} {...rest}>
+        <span className="icon">{render()}</span>
+        <p className="content">{children}</p>
+      </AlertStyled>
+    </div>
+  );
+};
+Alert.defaultProps = {
+  children: '',
+  type: 'success',
+  defineIcon: ''
+};
+
+export default Alert;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Alert from '../lib/Alert/Alert';
+import SuccessIcon from '../lib/icons/success.svg';
 
 const Wrap = styled.div`
   display: flex;
@@ -7,7 +9,32 @@ const Wrap = styled.div`
   gap: 50px;
 `;
 const Home: React.FC = () => {
-  return <Wrap>xxx</Wrap>;
+  return (
+    <Wrap>
+      <Alert type="success">
+        <span>Success Text</span>
+      </Alert>
+      <Alert type="info">
+        <span>Info Text</span>
+      </Alert>
+      <Alert type="warning">
+        <span>Warning Text</span>
+      </Alert>
+      <Alert type="error">
+        <span>Error Text</span>
+      </Alert>
+      <Alert type="error">
+        <span>Success Text</span>
+      </Alert>
+      <Alert defineIcon={<SuccessIcon width="1.5em" height="1.5em" />}>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab autem blanditiis consequuntur
+          corporis exercitationem ipsam, nihil nobis optio perspiciatis quas quibusdam repellendus,
+          sequi voluptate! Deleniti fuga pariatur qui ratione voluptatibus!
+        </span>
+      </Alert>
+    </Wrap>
+  );
 };
 
 export default Home;
