ca92dd805563ba5a46a816f669728fca529bd608
leigp <907090585@qq.com>
2022-07-26 12:05:46 +0800

    fix: 修复notification组件定位问题

---
+++ src/lib/Palette/example.tsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import styled from 'styled-components';
+// import Palette from '../lib/Palette/Palette';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 100px;
+//   justify-content: center;
+//   align-items: center;
+// `;
+//
+// const Home: React.FC = () => {
+//   return (
+//     <Wrap>
+//       <Palette />
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import Palette from '../lib/Palette/Palette';
+import Notification, { openNotification } from '../lib/Notification/Notification';
+
+import Button from '../lib/Button/Button';
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 100px;
-  justify-content: center;
-  align-items: center;
+  gap: 50px;
 `;
 
 const Home: React.FC = () => {
+  const open = () => {
+    openNotification(
+      <Notification>
+        <p>Notification Title</p>
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda cum ex
+          explicabo fuga, illo ipsa laudantium libero magni minus nobis nostrum, odio quam quibusdam
+          recusandae saepe soluta totam voluptates?
+        </div>
+      </Notification>,
+      3000
+    );
+  };
   return (
     <Wrap>
-      <Palette />
+      <div>
+        <Button onClick={open}>按钮</Button>
+      </div>
     </Wrap>
   );
 };
