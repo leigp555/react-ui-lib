e1cc3d86094de09e57f655eb7767d80b5d35ff63
leigp <907090585@qq.com>
2022-07-26 11:11:16 +0800

    feat: 添加empty组件示例

---
+++ src/lib/Empty/example.tsx
@@ -0,0 +1,48 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import styled from 'styled-components';
+// import Empty from '../lib/Empty/Empty';
+// import Button from '../lib/Button/Button';
+// import EmptyIcon from '../lib/icons/empty2.svg';
+// import EmptyIcon3 from '../lib/icons/empty3.svg';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 100px;
+//   justify-content: center;
+//   align-items: center;
+// `;
+//
+// const Home: React.FC = () => {
+//   return (
+//     <Wrap>
+//       <Empty
+//         icon={<EmptyIcon width="3em" height="3em" />}
+//         description={
+//           <p>
+//             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti dolores
+//             dolorum ducimus, eos, impedit
+//           </p>
+//         }
+//       >
+//         <Button type="primary">back</Button>
+//       </Empty>
+//       <Empty
+//         description={
+//           <p>
+//             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti dolores
+//             dolorum ducimus, eos, impedit
+//           </p>
+//         }
+//       >
+//         <Button type="primary">back</Button>
+//       </Empty>
+//       <Empty icon={<EmptyIcon3 width="3em" height="3em" />}>
+//         <Button type="primary">back</Button>
+//       </Empty>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Empty from '../lib/Empty/Empty';
-import Button from '../lib/Button/Button';
-import EmptyIcon from '../lib/icons/empty2.svg';
-import EmptyIcon3 from '../lib/icons/empty3.svg';
 
 const Wrap = styled.div`
   display: flex;
@@ -14,34 +10,7 @@ const Wrap = styled.div`
 `;
 
 const Home: React.FC = () => {
-  return (
-    <Wrap>
-      <Empty
-        icon={<EmptyIcon width="3em" height="3em" />}
-        description={
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti dolores
-            dolorum ducimus, eos, impedit
-          </p>
-        }
-      >
-        <Button type="primary">back</Button>
-      </Empty>
-      <Empty
-        description={
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam deleniti dolores
-            dolorum ducimus, eos, impedit
-          </p>
-        }
-      >
-        <Button type="primary">back</Button>
-      </Empty>
-      <Empty icon={<EmptyIcon3 width="3em" height="3em" />}>
-        <Button type="primary">back</Button>
-      </Empty>
-    </Wrap>
-  );
+  return <Wrap>xxx</Wrap>;
 };
 
 export default Home;
