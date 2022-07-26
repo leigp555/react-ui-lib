f281a5f97b240bb7321bfe7ae2f884bdcd7370b3
leigp <907090585@qq.com>
2022-07-27 07:14:00 +0800

    feat: 添加分割线组件示例

---
+++ src/lib/Divider/example.tsx
@@ -0,0 +1,64 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import styled from 'styled-components';
+// import Divider from '../lib/Divider/Divider';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 50px;
+// `;
+//
+// // 组件使用
+// const Home: React.FC = () => {
+//   return (
+//     <Wrap>
+//       <div>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus deleniti
+//           expedita inventore ipsum, magni pariatur vel. Accusantium, assumenda ea et iure, laborum
+//           nesciunt, omnis possimus quam quasi repellendus tempora?
+//         </p>
+//         <Divider position="left">Text</Divider>
+//       </div>
+//       <div>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus deleniti
+//           expedita inventore ipsum, magni pariatur vel. Accusantium, assumenda ea et iure, laborum
+//           nesciunt, omnis possimus quam quasi repellendus tempora?
+//         </p>
+//         <Divider position="left" offset={5}>
+//           Text
+//         </Divider>
+//       </div>
+//       <div>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus deleniti
+//           expedita inventore ipsum, magni pariatur vel. Accusantium, assumenda ea et iure, laborum
+//           nesciunt, omnis possimus quam quasi repellendus tempora?
+//         </p>
+//         <Divider position="center">Text</Divider>
+//       </div>
+//       <div>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus deleniti
+//           expedita inventore ipsum, magni pariatur vel. Accusantium, assumenda ea et iure, laborum
+//           nesciunt, omnis possimus quam quasi repellendus tempora?
+//         </p>
+//         <Divider offset={5} position="right">
+//           Text
+//         </Divider>
+//       </div>
+//       <div>
+//         <p>
+//           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid delectus deleniti
+//           expedita inventore ipsum, magni pariatur vel. Accusantium, assumenda ea et iure, laborum
+//           nesciunt, omnis possimus quam quasi repellendus tempora?
+//         </p>
+//         <Divider position="right">Text</Divider>
+//       </div>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
