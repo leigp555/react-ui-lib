65b3f2674dd337d08e14baedbb569ede6ea4833d
leigp <907090585@qq.com>
2022-07-29 13:56:34 +0800

    feat: 初始化分段控制组件

---
+++ src/lib/Segmented/Segmented.tsx
@@ -0,0 +1,30 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import Tab from '../Tabs/Tab';
+import Tabs from '../Tabs/Tabs';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  options: React.ReactNode[];
+}
+const SegmentedStyled = styled.div``;
+const Segmented: React.FC<Props> = (props) => {
+  const { children, options, ...rest } = props;
+  const onChange = (key: string) => {
+    console.log(key);
+  };
+  return (
+    <SegmentedStyled {...rest}>
+      <Tabs defaultKey="1" callback={onChange}>
+        {options.map((item, index) => {
+          return <Tab tab={item} index={(index + 1).toString()} key={Math.random()} />;
+        })}
+      </Tabs>
+    </SegmentedStyled>
+  );
+};
+Segmented.defaultProps = {
+  children: ''
+};
+
+export default Segmented;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,74 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import Timeline from '../lib/Timeline/Timeline';
-import TimeItem from '../lib/Timeline/TimeItem';
-import SunIcon from '../static/icons/sunny.svg';
+import Segmented from '../lib/Segmented/Segmented';
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   gap: 50px;
-  align-items: center;
+  align-items: start;
 `;
 
 // 组件使用
 const Home: React.FC = () => {
   return (
     <Wrap>
-      <Timeline mode="left">
-        <TimeItem color="red">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </TimeItem>
-        <TimeItem color="green">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem color="blue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </TimeItem>
-        <TimeItem color="orange">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem dotIcon={<SunIcon width="1em" height="1em" />}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-      </Timeline>
-      <Timeline mode="right">
-        <TimeItem color="red">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </TimeItem>
-        <TimeItem color="green">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem color="blue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </TimeItem>
-        <TimeItem color="orange">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem dotIcon={<SunIcon width="1em" height="1em" />}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-      </Timeline>
-      <Timeline mode="alternate">
-        <TimeItem color="red">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-        </TimeItem>
-        <TimeItem color="green">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem color="blue">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </TimeItem>
-        <TimeItem color="orange">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-        <TimeItem dotIcon={<SunIcon width="1em" height="1em" />}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.{' '}
-        </TimeItem>
-      </Timeline>
+      <Segmented options={['Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']} />
     </Wrap>
   );
 };
