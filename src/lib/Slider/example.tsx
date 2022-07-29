142dbfeee8addb8b3d3efaaa52a317a882079e5b
leigp <907090585@qq.com>
2022-07-30 06:48:22 +0800

    style: 添加slider组件示例

---
+++ src/lib/Slider/example.tsx
@@ -0,0 +1,54 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+// import Slider from '../lib/Slider/Slider';
+// import Progress from '../lib/Progress/Progress';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 50px;
+//   justify-content: start;
+//   align-items: start;
+//   > .row {
+//     display: flex;
+//     flex-direction: row;
+//     gap: 50px;
+//     justify-content: start;
+//     align-items: center;
+//   }
+// `;
+//
+// const Home: React.FC = () => {
+//   const [defaultPercent, setPercent] = useState<number>(20);
+//   return (
+//     <Wrap>
+//       <div className="row">
+//         <Slider
+//           defaultPercent={defaultPercent}
+//           setPercent={setPercent}
+//           style={{ width: '300px' }}
+//         />
+//       </div>
+//       <div className="row">
+//         <Progress
+//           percent={50}
+//           style={{ width: '300px', height: '8px' }}
+//           undoneColor="#e1e1e1"
+//           finishColor="#91d5ff"
+//           dot
+//         />
+//       </div>
+//       <div className="row">
+//         <Progress
+//           percent={50}
+//           style={{ width: '300px', height: '8px' }}
+//           undoneColor="#e1e1e1"
+//           finishColor="#91d5ff"
+//         />
+//       </div>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
