a61930b6e113cfe142bf1b4953e19242baedc8d6
leigp <907090585@qq.com>
2022-07-29 16:38:23 +0800

    feat: 完成Badge组件

---
+++ src/lib/Badge/Badge.tsx
@@ -0,0 +1,67 @@
+import React, { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import classNames from 'classnames';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  count?: React.ReactNode;
+  overflowCount?: number;
+  dot?: boolean;
+}
+const BadgeStyled = styled.div`
+  //background-color: #cccccc;
+  position: relative;
+  .badge {
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+    background-color: #df4402;
+    color: white;
+    padding: 3px 10px;
+    border-radius: 10px;
+    border: 2px solid white;
+    font-size: 12px;
+    &.dot {
+      padding: 0;
+      border-radius: 50%;
+      transform: translate(100%, -100%);
+      width: 4px;
+      height: 4px;
+      border: 4px solid red;
+    }
+  }
+  .badgeIcon {
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(50%, -50%);
+  }
+`;
+const Badge: React.FC<Props> = (props) => {
+  const { children, count, overflowCount, dot, ...rest } = props;
+  const render = () => {
+    if (typeof count === 'number') {
+      return (
+        <span className={classNames('badge', `${dot ? 'dot' : ''}`)}>
+          {count! > overflowCount! ? `${overflowCount}+` : count}
+        </span>
+      );
+    }
+    return <div className="badgeIcon">{count}</div>;
+  };
+  return (
+    <BadgeStyled {...rest}>
+      <div className="content">{children}</div>
+      <div>{render()}</div>
+    </BadgeStyled>
+  );
+};
+Badge.defaultProps = {
+  children: '',
+  count: 0,
+  overflowCount: 10,
+  dot: false
+};
+
+export default Badge;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,124 +1,64 @@
 import React from 'react';
 import styled from 'styled-components';
-import Tabs from '../lib/Tabs/Tabs';
-import Tab from '../lib/Tabs/Tab';
-import Warning from '../lib/icons/warn.svg';
+import Badge from '../lib/Badge/Badge';
+import Avatar from '../lib/Avatar/Avatar';
+import User from '../lib/icons/user.svg';
+import SunIcon from '../static/icons/sunny.svg';
 
 const Wrap = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 100px;
-  justify-content: center;
-  align-items: center;
+  gap: 50px;
+  justify-content: start;
+  align-items: start;
+  > .row {
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+    justify-content: start;
+    align-items: center;
+  }
 `;
 
 const Home: React.FC = () => {
-  const onChange = (key: string) => {
-    console.log(key);
-  };
   return (
     <Wrap>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange}>
-          <Tab tab="Tab 1" index="1">
-            Content of Tab Pane 1
-          </Tab>
-          <Tab tab="Tab 2" index="2">
-            Content of Tab Pane 2
-          </Tab>
-          <Tab tab="Tab 3" index="3">
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
+      <div className="row">
+        <Badge count={200} overflowCount={99}>
+          <div style={{ backgroundColor: '#ccc' }}>
+            <Avatar size={32}>
+              <User width="5em" height="5em" />
+            </Avatar>
+          </div>
+        </Badge>
+        <Badge count={200} overflowCount={10}>
+          <div style={{ backgroundColor: '#ccc' }}>
+            <Avatar size={32}>
+              <User width="5em" height="5em" />
+            </Avatar>
+          </div>
+        </Badge>
+        <Badge count={2000} overflowCount={999}>
+          <div style={{ backgroundColor: '#ccc' }}>
+            <Avatar size={32}>
+              <User width="5em" height="5em" />
+            </Avatar>
+          </div>
+        </Badge>
       </div>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange} bgc="orange">
-          <Tab tab="Tab 1" index="1">
-            Content of Tab Pane 1
-          </Tab>
-          <Tab tab="Tab 2" index="2">
-            Content of Tab Pane 2
-          </Tab>
-          <Tab tab="Tab 3" index="3">
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
+      <div className="row">
+        <Badge count={<SunIcon width="1.5em" height="1.5em" />}>
+          <div style={{ backgroundColor: '#ccc' }}>
+            <Avatar size={32}>
+              <User width="5em" height="5em" />
+            </Avatar>
+          </div>
+        </Badge>
       </div>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange} position="center">
-          <Tab tab="Tab 1" index="1">
-            Content of Tab Pane 1
-          </Tab>
-          <Tab tab="Tab 2" index="2">
-            Content of Tab Pane 2
-          </Tab>
-          <Tab tab="Tab 3" index="3">
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
-      </div>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange} gap={100}>
-          <Tab tab="Tab 1" index="1">
-            Content of Tab Pane 1
-          </Tab>
-          <Tab tab="Tab 2" index="2">
-            Content of Tab Pane 2
-          </Tab>
-          <Tab tab="Tab 3" index="3">
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
-      </div>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange} gap={100}>
-          <Tab
-            tab={
-              <>
-                <Warning width="1em" height="1em" />
-                <span>Tab 1</span>
-              </>
-            }
-            index="1"
-          >
-            Content of Tab Pane 1
-          </Tab>
-          <Tab
-            tab={
-              <>
-                <Warning width="1em" height="1em" />
-                <span>Tab 2</span>
-              </>
-            }
-            index="2"
-          >
-            Content of Tab Pane 2
-          </Tab>
-          <Tab
-            tab={
-              <>
-                <Warning width="1em" height="1em" />
-                <span>Tab 3</span>
-              </>
-            }
-            index="3"
-          >
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
-      </div>
-      <div style={{ border: '1px solid rgba(0,0,0,.1)', width: '100%' }}>
-        <Tabs defaultKey="1" callback={onChange}>
-          <Tab tab="Tab 1" index="1">
-            Content of Tab Pane 1
-          </Tab>
-          <Tab tab="Tab 2xxxxcccc" index="2">
-            Content of Tab Pane 2
-          </Tab>
-          <Tab tab="Tab 3xxxxcccccccccccccxxxxxxxxxx" index="3">
-            Content of Tab Pane 3
-          </Tab>
-        </Tabs>
+      <div className="row">
+        <Badge dot>
+          <p style={{ color: '#1890ff' }}>Lorem ipsum dolor sit amet</p>
+        </Badge>
       </div>
     </Wrap>
   );
