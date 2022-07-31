08886aef174d22c38c5e7d63559cf6e63a61c3a7
leigp <907090585@qq.com>
2022-07-31 15:59:01 +0800

    feat: 初始化自动完成组件

---
+++ src/lib/Calendar/example.tsx
@@ -0,0 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React from 'react';
+// import styled from 'styled-components';
+// import Calendar from '../lib/Calendar/Calendar';
+//
+// const Wrap = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 50px;
+//   justify-content: start;
+//   align-items: start;
+//   line-height: 1.5em;
+//   > .row {
+//     display: flex;
+//     flex-direction: row;
+//     gap: 50px;
+//     justify-content: center;
+//     align-items: center;
+//     width: 600px;
+//     height: 500px;
+//   }
+// `;
+//
+// const Home: React.FC = () => {
+//   return (
+//     <Wrap>
+//       <div className="row">
+//         <Calendar />
+//       </div>
+//     </Wrap>
+//   );
+// };
+//
+// export default Home;
