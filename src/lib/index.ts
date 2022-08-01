9ccdc87b4e416895ac3c3367535c1d15764e061e
leigp <907090585@qq.com>
2022-08-01 20:40:06 +0800

    feat: 统一组件导出

---
+++ src/lib/index.ts
@@ -0,0 +1,163 @@
+import Affix from './Affix/Affix';
+import Alert from './Alert/Alert';
+import AutoComplete from './AutoComplete/AutoComplete';
+import Avatar from './Avatar/Avatar';
+import BackTop from './BackTop/BackTop';
+import Badge from './Badge/Badge';
+import Breadcrumb from './Breadcrumb/Breadcrumb';
+import BreadcrumbItem from './Breadcrumb/BreadcrumbItem';
+import Button from './Button/Button';
+import Calendar from './Calendar/Calendar';
+import Card from './Card/Card';
+import Carousel from './Carousel/Carousel ';
+import CarouselItem from './Carousel/CarouselItem';
+import Checkbox from './Checkbox/Checkbox';
+import Collapse from './Collapse/Collapse';
+import Panel from './Collapse/Panel';
+import Comment from './Comment/Comment';
+import Divider from './Divider/Divider';
+import Drawer from './Drawer/Drawer';
+import Dropdown from './Dropdown/Dropdown ';
+import DropdownItem from './Dropdown/DropdownItem';
+import Empty from './Empty/Empty';
+import Form from './Form/Form';
+import FormItem from './Form/FormItem';
+import Col from './Grid/Col';
+import Row from './Grid/Row';
+import Image from './Image/Image';
+import Input from './Input/Input';
+import InputIcon from './Input/InputIcon';
+import Content from './Layout/Content';
+import Footer from './Layout/Footer';
+import Header from './Layout/Header';
+import Layout from './Layout/Layout';
+import LayoutWrap from './Layout/LayoutWrap';
+import Sider from './Layout/Sider';
+import List from './List/List';
+import ListItem from './List/ListItem';
+import Loading from './Loading/Loading';
+import ColumnSub from './Menu/ColumnSub';
+import ColumnMenu from './Menu/ColumnMenu';
+import ItemGroup from './Menu/ItemGroup';
+import Label from './Menu/Label';
+import Menu from './Menu/Menu ';
+import MenuItem from './Menu/MenuItem';
+import SubMenu from './Menu/SubMenu';
+import Modal from './Modal/Modal';
+import Notification from './Notification/Notification';
+import Paragraph from './Typography/Paragraph';
+import Palette from './Palette/Palette';
+import PopConfirm from './Popconfirm/Popconfirm';
+import Popover from './Popover/Popover';
+import Progress from './Progress/Progress';
+import Radio from './Radio/Radio';
+import RadioItem from './Radio/RadioItem';
+import Result from './Result/Result';
+import Segmented from './Segmented/Segmented';
+import Skeleton from './Skeleton/Skeleton';
+import SkeletonItem from './Skeleton/SkeletonItem';
+import Slider from './Slider/Slider';
+import Space from './Space/Space';
+import Step from './Steps/Step';
+import Steps from './Steps/Steps';
+import Switch from './Switch/Switch';
+import AsyncTable from './Table/AsyncTable';
+import Table from './Table/Table';
+import Tab from './Tabs/Tab';
+import Tabs from './Tabs/Tabs';
+import Tag from './Tag/Tag';
+import TagGroup from './Tag/TagGroup';
+import Timeline from './Timeline/Timeline';
+import TimeItem from './Timeline/TimeItem';
+import { Tip } from './Tip/Tip';
+import Tooltip from './Tooltip/Tooltip';
+import Tree from './Tree/Tree';
+import Pagination from './Pagination/Pagination ';
+import Text from './Typography/Text';
+import Title from './Typography/Title';
+import Typography from './Typography/Typography';
+import Upload from './Upload/Upload';
+import UploadToServer from './Upload/UploadToServer';
+
+export {
+  UploadToServer,
+  Upload,
+  Tooltip,
+  Tree,
+  Pagination,
+  Text,
+  Title,
+  Typography,
+  Tip,
+  Tab,
+  Tag,
+  Tabs,
+  TagGroup,
+  TimeItem,
+  Timeline,
+  Segmented,
+  SkeletonItem,
+  Skeleton,
+  Slider,
+  Space,
+  Steps,
+  Step,
+  Switch,
+  AsyncTable,
+  Table,
+  Modal,
+  Notification,
+  Paragraph,
+  Palette,
+  PopConfirm,
+  Popover,
+  Progress,
+  Radio,
+  RadioItem,
+  Result,
+  SubMenu,
+  MenuItem,
+  Menu,
+  Label,
+  ItemGroup,
+  ColumnMenu,
+  ColumnSub,
+  Loading,
+  ListItem,
+  List,
+  Content,
+  Footer,
+  Header,
+  Layout,
+  LayoutWrap,
+  Sider,
+  Image,
+  Input,
+  InputIcon,
+  Form,
+  FormItem,
+  Col,
+  Row,
+  Empty,
+  DropdownItem,
+  Dropdown,
+  Drawer,
+  Divider,
+  Comment,
+  Panel,
+  Collapse,
+  Checkbox,
+  CarouselItem,
+  Carousel,
+  Card,
+  Affix,
+  Alert,
+  AutoComplete,
+  Avatar,
+  BackTop,
+  Badge,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Calendar
+};
